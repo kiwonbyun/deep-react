954b07b797554adf7dd1b7859927b94f68058a7d
변기원 <byunkiwon@byeongiwon-ui-MacBookPro.local>
2024-07-18 19:20:58 +0900

    asd

--- app.js
+++ app.js
@@ -1,23 +1,83 @@
 const rootNode = document.getElementById("app");
 const root = ReactDOM.createRoot(rootNode);
 
-root.render(React.createElement(App));
+let counterName = "one";
+
+root.render(<App />);
 
 function App() {
-  return React.createElement(
-    "section",
-    null,
-    React.createElement("h1", null, "Counters"),
-    React.createElement("section", null, React.createElement(Counter, null))
+  return (
+    <section>
+      <h1>Counters</h1>
+      <section>
+        <Counter />
+      </section>
+    </section>
   );
 }
 
-function Counter() {
-  return React.createElement(
-    "article",
-    null,
-    React.createElement("h2", null, "Counter "),
-    React.createElement("p", null, "You clicked 1 times"),
-    React.createElement("button", { className: "bububu" }, "Click me")
+function Counter({ name }) {
+  return (
+    <article>
+      <h2>Counter {name}</h2>
+      <p>you clicked 1 times</p>
+      <button className="button">Click me</button>
+    </article>
   );
 }
+
+function Counter2({ name }) {
+  return (
+    <article>
+      <h2>Counter {name}</h2>
+      <p>Times clicked : 1</p>
+      <button className="button">Click me</button>
+    </article>
+  );
+}
+
+function rerender() {
+  console.log("Rerender...");
+  counterName = "two";
+  root.render(React.createElement(App));
+}
+
+// class LinkedListNode {
+//   constructor(val, next = null) {
+//     this.value = val;
+//     this.next = next;
+//   }
+// }
+
+//   constructor() {
+//     this.head = null;
+//     this.tail = null;
+//   }
+
+//   append(val) {
+//     const newNode = new LinkedListNode(val);
+
+//     if (!this.head) {
+//       this.head = newNode;
+//       this.tail = newNode;
+//       return this;
+//     }
+
+//     this.tail.next = newNode;
+//     this.tail = newNode;
+//   }
+
+//   print() {
+//     let currentNode = this.head;
+//     while (currentNode) {
+//       console.log(currentNode.value);
+//       currentNode = currentNode.next;
+//     }
+//   }
+// }
+
+// let myLinkedList = new LinkedList();
+// myLinkedList.append("tony");
+// myLinkedList.append("alicea");
+// myLinkedList.append("understanding React");
+// myLinkedList.print();
