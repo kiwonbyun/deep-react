0fb3ee09970bcdcb0f5e84fd0f982f046455fd11
변기원 <byunkiwon@byeongiwon-ui-MacBookPro.local>
2024-07-19 18:14:39 +0900

    sadkj

--- app.js
+++ app.js
@@ -10,74 +10,34 @@ function App() {
     <section>
       <h1>Counters</h1>
       <section>
-        <Counter />
+        <Counter name="one" />
       </section>
     </section>
   );
 }
 
 function Counter({ name }) {
+  const [state, dispath] = React.useReducer(
+    (state, action) => {
+      switch (action.type) {
+        case "Increment":
+          return { clicks: state.clicks + 1 };
+        default:
+          throw new Error();
+      }
+    },
+    {
+      clicks: 0,
+    }
+  );
+
   return (
     <article>
       <h2>Counter {name}</h2>
-      <p>you clicked 1 times</p>
-      <button className="button">Click me</button>
+      <p>you clicked {state.clicks} times</p>
+      <button className="button" onClick={() => dispath({ type: "Increment" })}>
+        Click me
+      </button>
     </article>
   );
 }
-
-function Counter2({ name }) {
-  return (
-    <article>
-      <h2>Counter {name}</h2>
-      <p>Times clicked : 1</p>
-      <button className="button">Click me</button>
-    </article>
-  );
-}
-
-function rerender() {
-  console.log("Rerender...");
-  counterName = "two";
-  root.render(React.createElement(App));
-}
-
-// class LinkedListNode {
-//   constructor(val, next = null) {
-//     this.value = val;
-//     this.next = next;
-//   }
-// }
-
-//   constructor() {
-//     this.head = null;
-//     this.tail = null;
-//   }
-
-//   append(val) {
-//     const newNode = new LinkedListNode(val);
-
-//     if (!this.head) {
-//       this.head = newNode;
-//       this.tail = newNode;
-//       return this;
-//     }
-
-//     this.tail.next = newNode;
-//     this.tail = newNode;
-//   }
-
-//   print() {
-//     let currentNode = this.head;
-//     while (currentNode) {
-//       console.log(currentNode.value);
-//       currentNode = currentNode.next;
-//     }
-//   }
-// }
-
-// let myLinkedList = new LinkedList();
-// myLinkedList.append("tony");
-// myLinkedList.append("alicea");
-// myLinkedList.append("understanding React");
-// myLinkedList.print();
